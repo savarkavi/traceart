782ab67e36382203009145330f44e04d96780682
Sushant Sharma <sushant20.sharma00@gmail.com>
2026-08-15 07:28:14 +0530

    feat: configure image remote patterns for external hosting in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "outstanding-perch-546.convex.cloud",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
