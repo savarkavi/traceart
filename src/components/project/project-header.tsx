b63f784f665a104c154db848877b8dbffd98ee9b
Sushant Sharma <sushant20.sharma00@gmail.com>
2026-09-08 05:57:04 +0530

    feat: add publish/unpublish functionality with loading state and update button UI in ProjectHeader

--- src/components/project/project-header.tsx
+++ src/components/project/project-header.tsx
@@ -5,15 +5,54 @@ import type { Doc } from "../../../convex/_generated/dataModel";
 
 import ProjectHeaderTabs from "./project-header-tabs";
 import { Button } from "../ui/button";
-import { Pencil, Upload } from "lucide-react";
+import { Globe, GlobeOff, Loader, Pencil, Upload } from "lucide-react";
 import UploadVersionButton from "./upload-version-button";
+import { cn } from "@/lib/utils";
+import { useMutation } from "convex/react";
+import { api } from "../../../convex/_generated/api";
+import { toast } from "../ui/toast";
+import { useState } from "react";
 
 type ProjectHeaderProps = {
   project: Doc<"projects">;
 };
 
 export default function ProjectHeader({ project }: ProjectHeaderProps) {
+  const [isPublishing, setIsPublishing] = useState(false);
+
   const projectId = project._id;
+  const publishProject = useMutation(api.project.publishProject);
+  const unpublishProject = useMutation(api.project.unpublishProject);
+
+  const handlePublish = async () => {
+    setIsPublishing(true);
+
+    try {
+      if (project.isPublic) {
+        await unpublishProject({ projectId });
+        toast.add({
+          type: "success",
+          description: "Project Unpublished",
+        });
+      } else {
+        await publishProject({ projectId });
+
+        toast.add({
+          type: "success",
+          description: "Project published",
+        });
+      }
+    } catch (error) {
+      console.log(error);
+      toast.add({
+        type: "error",
+        description:
+          error instanceof Error ? error.message : "Failed to publish.",
+      });
+    } finally {
+      setIsPublishing(false);
+    }
+  };
 
   return (
     <section>
@@ -42,6 +81,31 @@ export default function ProjectHeader({ project }: ProjectHeaderProps) {
               Edit details
             </Button>
           </Link>
+          <Button
+            className={cn(
+              "py-3.5 text-white",
+              project.isPublic
+                ? "bg-primary/20 border-primary hover:bg-primary/50"
+                : "bg-input/30 border-input hover:bg-input/50",
+            )}
+            onClick={handlePublish}
+            disabled={isPublishing}
+          >
+            {isPublishing ? (
+              <Loader className="text-muted-foreground size-3.5 animate-spin" />
+            ) : project.isPublic ? (
+              <GlobeOff className="text-muted-foreground size-3.5" />
+            ) : (
+              <Globe className="text-muted-foreground size-3.5" />
+            )}
+            {isPublishing
+              ? project.isPublic
+                ? "Unpublishing..."
+                : "Publishing..."
+              : project.isPublic
+                ? "Unpublish"
+                : "Publish"}
+          </Button>
           <UploadVersionButton
             projectId={projectId}
             classNames="bg-primary text-primary-foreground hover:bg-primary/90 inline-flex py-2 shrink-0 items-center gap-2 rounded-lg px-3 text-sm shadow-sm transition-all active:translate-y-px cursor-pointer"
